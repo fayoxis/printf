1d37c2737b18e3ddd160c3f280a7a31bb7157a63
Samuel Sylvester <samsylvester09@gmail.com>
2023-11-15 03:51:24 +0100

    Create specifier3.c

---
+++ specifier3.c
@@ -0,0 +1,146 @@
+#include "main.h"
+
+int printPointer(va_list args, char buffer[],
+int flags, int width, int precision, int size)
+{
+char extraChar = 0, paddingChar = ' ';
+int index = BUFF_SIZE - 2, length = 2;
+int paddingStart = 1; /* length=2 for '0x' */
+unsigned long numAddrs;
+char mapTo[] = "0123456789abcdef";
+void *address = va_arg(args, void *);
+
+UNUSED(width);
+UNUSED(precision);
+UNUSED(size);
+
+if (address == NULL)
+return ((write(1, "(nil)", 5)));
+buffer[BUFF_SIZE - 1] = '\0';
+numAddrs = (unsigned long)address;
+do {
+buffer[index--] = mapTo[numAddrs % 16];
+numAddrs /= 16;
+length++;
+}
+while (numAddrs > 0);
+if ((flags & F_ZERO) && !(flags & F_MINUS))
+paddingChar = '0';
+if (flags & F_PLUS)
+extraChar = '+', length++;
+else if (flags & F_SPACE)
+extraChar = ' ', length++;
+index++;
+return ((writeMemoryAddress(buffer, index, length,
+width, flags, paddingChar,
+extraChar, paddingStart)));
+}
+int printNonPrintable(va_list args, char buffer[],
+int flags, int width, int precision, int size)
+{
+int i = 0, offset = 0;
+char *str = va_arg(args, char *);
+UNUSED(width);
+UNUSED(flags);
+UNUSED(buffer);
+UNUSED(precision);
+UNUSED(size);
+if (str == NULL)
+return (write(1, "(null)", 6));
+while (str[i] != '\0')
+{
+if (isCharacterPrintable(str[i]))
+buffer[i + offset] = str[i];
+else
+offset += hexadecimal_code(str[i], buffer, i + offset);
+i++;
+if (i + offset >= BUFF_SIZE - 1)
+{
+write(1, buffer, i + offset);
+offset = 0;
+i = 0;
+}
+}
+buffer[i + offset] = '\0';
+return (write(1, buffer, i + offset));
+}
+int reverse_print(va_list args, char buffer[],
+int flags, int width, int precision, int size)
+{
+char *str;
+int i, count = 0;
+UNUSED(buffer);
+UNUSED(flags);
+UNUSED(width);
+UNUSED(size);
+str = va_arg(args, char *);
+if (str == NULL)
+{
+UNUSED(precision);
+str = ")Null(";
+}
+if (precision == -1 && str[0] == '%' && str[1] == 'r') {
+for (i = count - 1; i >= 0; i--)
+{
+char z = buffer[i];
+_putchar(z);
+}
+return count;
+}
+for (i = 0; str[i]; i++)
+;
+for (i = i - 1; i >= 0; i--)
+{
+char z = str[i];
+_putchar(z);
+count++;
+}
+	return count;
+}
+int p_rot13string(va_list args, char buffer[],
+                   int flags, int width, int precision, int size)
+{
+    char *str;
+    unsigned int i, j, k;
+    int count = 0;
+    char in[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+    char out[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+    UNUSED(flags);
+    UNUSED(width);
+    UNUSED(precision);
+    UNUSED(size);
+    UNUSED(buffer);
+
+    str = va_arg(args, char *);
+
+    if (str == NULL)
+        str = "(NULL)";
+
+    for (i = 0; str[i]; i++)
+    {
+        for (j = 0; in[j]; j++)
+        {
+            if (in[j] == str[i])
+            {
+                char x = out[j];
+                for (k = 0; k < 1; k++)
+                {
+                    write(1, &x, 1); /* Write character to output */
+                    count++;
+                }
+                break;
+            }
+        }
+        if (!in[j])
+        {
+            char x = str[i];
+            for (k = 0; k < 1; k++)
+            {
+                write(1, &x, 1); /* Write character to output */
+                count++;
+            }
+        }
+    }
+    return count;
+}
