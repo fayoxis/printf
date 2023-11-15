d3ad169741ee7e3136b824d28e54dca92baa2654
Samuel Sylvester <samsylvester09@gmail.com>
2023-11-15 03:52:32 +0100

    Create width.c

---
+++ width.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * calculatePrintWidth - Calculates the width value for printing
+ * @formatString: The formatted string in which the arguments will be printed.
+ * @currentIndex: The index of the current character in the format string.
+ * @argList: The list of arguments.
+ *
+ * Return: The calculated print width.
+ */
+
+int calculatePrintWidth(const char *formatString,
+		int *currentIndex, va_list argList)
+{
+	int printWidth = 0;
+
+	while (isCharacterDigit(formatString[*currentIndex + 1]))
+	{
+		printWidth *= 10;
+		printWidth += formatString[*currentIndex + 1] - '0';
+		(*currentIndex)++;
+	}
+
+	if (formatString[*currentIndex + 1] == '*')
+	{
+		(*currentIndex)++;
+		printWidth = va_arg(argList, int);
+	}
+
+	return (printWidth);
+}
