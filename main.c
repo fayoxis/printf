58ca665c1d0a0623a264ef2efcc8a68c71ad15c0
Samuel Sylvester <samsylvester09@gmail.com>
2023-11-15 04:21:10 +0100

    Update main.c

--- main.c
+++ main.c
@@ -38,8 +38,8 @@ int main(void)
     len2 = printf("Percent:[%%]\n");
     _printf("Len:[%d]\n", len);
     printf("Len:[%d]\n", len2);
-    _printf("Unknown:[%r]\n");
-    printf("Unknown:[%r]\n");
+    _printf("Unknown:[%y]\n");
+    printf("Unknown:[%y]\n");
     _printf("%b\n", 98);
     return (0);
 }
