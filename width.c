f2d4070e0f304a2f69455ca3f27560a8bab346f2
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2023-11-16 13:05:58 +0100

    Update width.c

--- width.c
+++ width.c
@@ -10,26 +10,24 @@
  */
 
 int calculatePrintWidth(const char *formatString,
-                        int *currentIndex, va_list argList)
+		int *currentIndex, va_list argList)
 {
-    int printWidth = 0;
+	int printWidth = 0;
 
-    while (isCharacterDigit(formatString[*currentIndex + 1]))
-    {
-        printWidth *= 10;
-        printWidth += formatString[*currentIndex + 1] - '0';
-        (*currentIndex)++;
-    }
+	while (isCharacterDigit(formatString[*currentIndex + 1]))
+	{
+		printWidth *= 10;
+		printWidth += formatString[*currentIndex + 1] - '0';
+		(*currentIndex)++;
+	}
 
-    if (formatString[*currentIndex + 1] == '*')
-    {
-        (*currentIndex)++;
-        printWidth = va_arg(argList, int);
-    }
+	if (formatString[*currentIndex + 1] == '*')
+	{
+		(*currentIndex)++;
+		printWidth = va_arg(argList, int);
+	}
 
-    _printf("Print width: %d\n", printWidth); 
-
-    return printWidth;
+	return (printWidth);
 }
 
 
