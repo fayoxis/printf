ea73f404fd6438c683651906c152a07d9419d57a
Samuel Sylvester <samsylvester09@gmail.com>
2023-11-17 20:36:17 +0100

    Update formatNumber.c

--- formatNumber.c
+++ formatNumber.c
@@ -1,16 +1,16 @@
 #include "main.h"
 
 /**
- * printFormattedNumber - Prints a formatted number
- * @isNegative: Indicates whether the number is negative
- * @index: Index position in the buffer array
- * @buffer: Buffer array for printing
- * @flags: Active flags for formatting
- * @width: Width specifier
- * @precision: Precision specifier
- * @size: Size specifier
- * Return: Number of characters printed
- **/
+* printFormattedNumber - Prints a formatted number
+* @isNegative: Indicates whether the number is negative
+* @index: Index position in the buffer array
+* @buffer: Buffer array for printing
+* @flags: Active flags for formatting
+* @width: Width specifier
+* @precision: Precision specifier
+* @size: Size specifier
+* Return: Number of characters printed
+**/
 int printFormattedNumber(int isNegative, int index, char buffer[],
 int flags, int width, int precision, int size)
 {
@@ -60,38 +60,40 @@ buffer[start_index] == '0') {
 if (width == 0)
 return (0);
 buffer[start_index] = padding_char = ' ';
-    }
-    if (precision > 0 && precision < num_length)
-        padding_char = ' ';
-    while (precision > num_length)
-        buffer[--start_index] = '0', num_length++;
-    if (extra_char != 0)
-        num_length++;
-    if (width > num_length) {
-        for (i = 0; i < padding_count; i++)
-            padding_buffer[i] = padding_char;
-        padding_buffer[i] = '\0';
-        if (flags & F_MINUS && padding_char == ' ') {
-            if (extra_char)
-                buffer[--start_index] = extra_char, num_length++;
-            printed_chars += write(1, &buffer[start_index], num_length);
-            printed_chars += write(1, padding_buffer, padding_count);
-        } else {
-            if (extra_char) {
-                if (padding_char == '0')
-                    buffer[--padding_start] = extra_char;
-                else
-                    buffer[--start_index] = extra_char, num_length++;
-            }
-            printed_chars += write(1, padding_buffer, padding_count);
-            printed_chars += write(1, &buffer[start_index], num_length);
-        }
-        return (printed_chars);
-    }
-    if (extra_char)
-        buffer[--start_index] = extra_char;
-    printed_chars = write(1, &buffer[start_index], num_length);
-    return (printed_chars);
+}
+if (precision > 0 && precision < num_length)
+padding_char = ' ';
+while (precision > num_length)
+buffer[--start_index] = '0', num_length++;
+if (extra_char != 0)
+num_length++;
+if (width > num_length) {
+for (i = 0; i < padding_count; i++)
+padding_buffer[i] = padding_char;
+padding_buffer[i] = '\0';
+if (flags & F_MINUS && padding_char == ' ') {
+if (extra_char)
+buffer[--start_index] = extra_char, num_length++;
+printed_chars += write(1, &buffer[start_index], num_length);
+printed_chars += write(1, padding_buffer, padding_count);
+}
+else
+{
+if (extra_char) {
+if (padding_char == '0')
+buffer[--padding_start] = extra_char;
+else
+buffer[--start_index] = extra_char, num_length++;
+}
+printed_chars += write(1, padding_buffer, padding_count);
+printed_chars += write(1, &buffer[start_index], num_length);
+}
+return (printed_chars);
+}
+if (extra_char)
+buffer[--start_index] = extra_char;
+printed_chars = write(1, &buffer[start_index], num_length);
+return (printed_chars);
 }
 
 /**
