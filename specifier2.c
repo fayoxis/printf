8e306c4661c16f334c1b1c51924d661264d6fc78
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2023-11-18 05:06:43 +0100

    Update specifier2.c

--- specifier2.c
+++ specifier2.c
@@ -22,13 +22,13 @@ number = convert_number(number, sizeSpecifier);
 if (number == 0)
 outputBuffer[bufferIndex--] = '0';
 outputBuffer[BUFF_SIZE - 1] = '\0';
-do
-{
+do {
 outputBuffer[bufferIndex--] = (number % 10) + '0';
 number /= 10;
 } while (number > 0);
 bufferIndex++;
-return (writeUnsignedNumber(0, bufferIndex, outputBuffer, flags, width, precision, sizeSpecifier));
+return (writeUnsignedNumber(0, bufferIndex,
+outputBuffer, flags, width, precision, sizeSpecifier));
 }
 
 /**
@@ -39,9 +39,8 @@ return (writeUnsignedNumber(0, bufferIndex, outputBuffer, flags, width, precisio
  * @widthSpec: Width specification
  * @precisionSpec: Precision specification
  * @sizeSpec: Size specifier
- * 
  * Return: Number of characters printed
- */
+ **/
 
 int printOctal(va_list argList, char outputBuffer[],
 int calcFlags, int widthSpec, int precisionSpec, int sizeSpec)
@@ -54,32 +53,34 @@ num = convert_number(num, sizeSpec);
 if (num == 0)
 outputBuffer[i--] = '0';
 outputBuffer[BUFF_SIZE - 1] = '\0';
-do
-{
+do {
 outputBuffer[i--] = (num % 8) + '0';
 num /= 8;
 } while (num > 0);
 if (calcFlags & F_HASH && initNum != 0)
 outputBuffer[i--] = '0';
 i++;
-return (writeUnsignedNumber(0, i, outputBuffer, calcFlags, widthSpec, precisionSpec, sizeSpec));
+return (writeUnsignedNumber(0, i, outputBuffer,
+calcFlags, widthSpec, precisionSpec, sizeSpec));
 }
 
 /**
- * print_Hexadecimal_num - Outputs an unsigned number using hexadecimal notation
+ * print_Hexadecimal_num - Outputs an unsigned
+ * number using hexadecimal notation
  * @types: List of arguments containing the number to be printed
  * @buffer: Array used as a buffer for printing
  * @flags: Active flag calculation for formatting
  * @width: Width specification for formatting
  * @precision: Precision specification for formatting
  * @size: Size specifier for formatting
- * 
  * Returns: The count of characters printed
  */
-int print_Hexadecimal_num(va_list types, char buffer[], int flags, int width, int precision, int size) {
-return (printed_Hexadecimal(types, "0123456789abcdef", buffer, flags, 'x', width, precision, size));
+int print_Hexadecimal_num(va_list types, char buffer[],
+int flags, int width, int precision, int size)
+{
+return (printed_Hexadecimal(types, "0123456789abcdef",
+buffer, flags, 'x', width, precision, size));
 }
-
 
 /**
  * print_hexadecimal_upper - Prints an unsigned number
@@ -125,8 +126,7 @@ number = convert_number(number, size);
 if (number == 0)
 outputBuffer[i--] = '0';
 outputBuffer[BUFF_SIZE - 1] = '\0';
-do
-{
+do {
 outputBuffer[i--] = mapping[number % 16];
 number /= 16;
 } while (number > 0);
@@ -136,5 +136,6 @@ outputBuffer[i--] = flagChar;
 outputBuffer[i--] = '0';
 }
 i++;
-return (writeUnsignedNumber(0, i, outputBuffer, flags, width, precision, size));
+return (writeUnsignedNumber(0, i, outputBuffer,
+flags, width, precision, size));
 }
